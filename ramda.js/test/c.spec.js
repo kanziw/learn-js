cea91005edb995b2bbaa5fe7151331c91b193a61
kanziw <kanziwoong@gmail.com>
2018-06-10 21:24:27 +0900

    [ramda] R.chain

--- ramda.js/test/c.spec.js
+++ ramda.js/test/c.spec.js
@@ -21,3 +21,18 @@ describe('call', () => {
     expect(add1(2)).eql(3)
   })
 })
+
+describe('chain', function () {
+  /**
+   * chain maps a function over a list and concatenates the results. chain is also known as flatMap in some libraries
+   * Dispatches to the chain method of the second argument, if present, according to the FantasyLand Chain spec.
+   *
+   * 다른 라이브러리에서 일명 flatMap 이라 불리는 기능을 수행한다.
+   */
+  it('simple', () => {
+    const duplicate = n => [ n, n ]
+    expect(R.chain(duplicate, [ 1, 2, 3 ])).eql([ 1, 1, 2, 2, 3, 3 ])
+    expect(R.chain(R.append, R.head)([ 1, 2, 3 ])).eql([ 1, 2, 3, 1 ])
+    expect(R.chain(R.append, R.last)([ 1, 2, 3 ])).eql([ 1, 2, 3, 3 ])
+  })
+})
