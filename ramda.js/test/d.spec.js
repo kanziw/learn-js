ab342f29bd163cbab529680b467b4debe26f9ca3
kanziw <kanziwoong@gmail.com>
2018-06-12 22:01:26 +0900

    [ramda] R.descend

--- ramda.js/test/d.spec.js
+++ ramda.js/test/d.spec.js
@@ -20,3 +20,14 @@ describe('defaultTo', () => {
     expect(defaultTo42('Ramda')).eql('Ramda')
   })
 })
+
+describe('descend', () => {
+  /**
+   * Makes a descending comparator function out of a function that returns a value that can be compared with < and >.
+   * 내림차순으로 정렬하기 위해 사용될 comparator function 을 반환한다.
+   */
+  it('simple', () => {
+    expect([ 1, 3, 0, 2, 5, 4 ].sort(R.descend(R.identity))).eql([ 5, 4, 3, 2, 1, 0 ])
+    expect([ { a: 5 }, { a: 0 } ].sort(R.descend(x => x.a))).eql([ { a: 5 }, { a: 0 } ])
+  })
+})
