de58b87bba9de7f6a5385da96fbb1c86f227a2c5
kanziw <kanziwoong@gmail.com>
2018-06-12 22:08:37 +0900

    [ramda] R.difference, R.differenceWith

--- ramda.js/test/d.spec.js
+++ ramda.js/test/d.spec.js
@@ -31,3 +31,35 @@ describe('descend', () => {
     expect([ { a: 5 }, { a: 0 } ].sort(R.descend(x => x.a))).eql([ { a: 5 }, { a: 0 } ])
   })
 })
+
+describe('difference', () => {
+  /**
+   * Finds the set (i.e. no duplicates) of all elements in the first list not contained in the second list.
+   * Objects and Arrays are compared in terms of value equality, not reference equality.
+   *
+   * Objects 와 Arrays 를 서로 값 비교하여 차집합을 구한다.
+   */
+  it('simple', () => {
+    expect(R.difference([ 1, 2, 3, 4 ], [ 7, 6, 5, 4, 3 ])).eql([ 1, 2 ])
+    expect(R.difference([ 7, 6, 5, 4, 3 ], [ 1, 2, 3, 4 ])).eql([ 7, 6, 5 ])
+    expect(R.difference([ { a: 1 }, { b: 2 } ], [ { a: 1 }, { c: 3 } ])).eql([ { b: 2 } ])
+  })
+
+  it('curry', () => {
+    const curry = R.difference([ 1, 2, 3, 4 ])
+    expect(curry([ 2, 3, 4 ])).eql([ 1 ])
+    expect(curry([ 3, 4 ])).eql([ 1, 2 ])
+    expect(curry([ 4 ])).eql([ 1, 2, 3 ])
+  })
+})
+
+describe('differenceWith', () => {
+  it('simple', () => {
+    const cmp = (x, y) => x.a === y.a - 1
+    const l1 = [ { a: 1 }, { a: 2 }, { a: 3 } ]
+    const l2 = [ { a: 3 }, { a: 4 } ]
+
+    expect(R.differenceWith(cmp, l1, l2)).eql([ { a: 1 } ])
+    expect(R.difference(l1, l2)).eql([ { a: 1 }, { a: 2 } ])
+  })
+})
