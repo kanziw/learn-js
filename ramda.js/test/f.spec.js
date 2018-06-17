4de446a9cdfcccab8e331e2858f6c1b24dfa5b1b
kanziw <kanziwoong@gmail.com>
2018-06-17 21:26:21 +0900

    [ramda] R.filter

--- ramda.js/test/f.spec.js
+++ ramda.js/test/f.spec.js
@@ -6,3 +6,24 @@ describe('F', () => {
     expect(R.F()).to.be.false
   })
 })
+
+describe('filter', () => {
+  /**
+   * Object 가 평가 할 대상으로 들어오면 value 를 통과한 Object 를 반환한다.
+   */
+
+  const testArr = [ 1, 2, 3, 4 ]
+  const testObj = { a: 1, b: 2, c: 3, d: 4 }
+  const evenArr = [ 2, 4 ]
+  const evenObj = { b: 2, d: 4 }
+  const isEven = n => n % 2 === 0
+
+  it('simple & curry', () => {
+    expect(R.filter(isEven, testArr)).eql(evenArr)
+    expect(R.filter(isEven, testObj)).eql(evenObj)
+
+    const filterEven = R.filter(isEven)
+    expect(filterEven(testArr)).eql(evenArr)
+    expect(filterEven(testObj)).eql(evenObj)
+  })
+})
