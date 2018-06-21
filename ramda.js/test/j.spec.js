c077e6682dd0a594326b59a416797d2309aa4502
kanziw <kanziwoong@gmail.com>
2018-06-22 00:21:50 +0900

    [ramda] R.juxt

--- ramda.js/test/j.spec.js
+++ ramda.js/test/j.spec.js
@@ -10,3 +10,12 @@ describe('join', () => {
     expect(R.join('/')([ 3, 'a', 'b', 1, 1.2 ])).eql('3/a/b/1/1.2')
   })
 })
+
+describe('juxt', () => {
+  it('simple', () => {
+    const sumArgs = (...args) => R.sum(args)
+    const minusArgs = (...args) => R.reduce(R.subtract, 0, args)
+
+    expect(R.juxt([ Math.min, Math.max, sumArgs, minusArgs ])(3, 4, 9, -3)).eql([ -3, 9, 13, -13 ])
+  })
+})
