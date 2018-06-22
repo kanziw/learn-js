e092114b3e94722c4e91441ff9c1b3f54b24cac8
kanziw <kanziwoong@gmail.com>
2018-06-22 23:29:03 +0900

    [joi] optional option

--- joi/test/joi.spec.js
+++ joi/test/joi.spec.js
@@ -1,6 +1,16 @@
 const Joi = require('joi')
 const R = require('ramda')
+const Promise = require('bluebird')
 const expect = require('chai').expect
+
+const catchAsyncError = async promise => {
+  try {
+    await Promise.resolve(promise)
+    expect(true).to.be.false
+  } catch (ex) {
+    return ex
+  }
+}
 
 describe('validate', () => {
   let [ retErr, retValue ] = []
@@ -44,6 +54,36 @@ describe('validate', () => {
       expect(retValue).eql({ num: 1 })
       expect(retErr).eql({ num: 1 })    // catch also return value
       expect(hasError).to.be.false      // catch scope does not be reached
+    })
+  })
+
+  describe('optional', () => {
+    const schema = {
+      num: Joi.number(),
+      str: Joi.string(),
+    }
+
+    it('simple', async () => {
+      const testSet = [ {}, { num: 1 }, { num: 1, str: 'a' } ]
+      await Promise.each(testSet, obj => Joi.validate(obj, schema).then(v => expect(v).eql(obj)))
+    })
+
+    it('error', async () => {
+      const err = await catchAsyncError(Joi.validate({ num: 'a' }, schema))
+      expect(R.keys(err)).eql([ 'isJoi', 'name', 'details', '_object', 'annotate' ])
+
+      expect(err.isJoi).to.be.true
+      expect(err.name).eql('ValidationError')
+      expect(err._object).eql({ num: 'a' })
+      expect(err.annotate).to.be.an('function')
+
+      expect(err.details).to.be.an.instanceOf(Array).lengthOf(1)
+      expect(err.details[ 0 ]).eql({
+        message: '"num" must be a number',
+        path: [ 'num' ],
+        type: 'number.base',
+        context: { key: 'num', label: 'num' }
+      })
     })
   })
 })
