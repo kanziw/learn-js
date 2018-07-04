96c5246b5d8718fdc4f51b4223ba0a13685adf17
kanziw <kanziwoong@gmail.com>
2018-07-04 20:57:45 +0900

    [graphql] first query, name

---
+++ graphql/graphql/resolvers.js
@@ -0,0 +1,7 @@
+const resolvers = {
+  Query: {
+    name: () => 'kanziw',
+  },
+}
+
+export default resolvers
--- graphql/index.js
+++ graphql/index.js
@@ -1,4 +1,8 @@
 import { GraphQLServer } from 'graphql-yoga'
+import resolvers from './graphql/resolvers'
 
-const server = GraphQLServer({})
+const server = new GraphQLServer({
+  typeDefs: 'graphql/schema.graphql',
+  resolvers,
+})
 server.start(() => console.log('GraphQL Server Running!'))
