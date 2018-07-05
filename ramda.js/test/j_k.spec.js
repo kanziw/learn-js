0fc9eeb5170a78867d82391bd643720e0a44e5f4
kanziw <kanziwoong@gmail.com>
2018-07-05 21:44:07 +0900

    [ramda] R.keys, R.keysIn

--- ramda.js/test/j_k.spec.js
+++ ramda.js/test/j_k.spec.js
@@ -19,3 +19,35 @@ describe('juxt', () => {
     expect(R.juxt([ Math.min, Math.max, sumArgs, minusArgs ])(3, 4, 9, -3)).eql([ -3, 9, 13, -13 ])
   })
 })
+
+describe('keys', () => {
+  /**
+   * keys only return properties, not prototype
+   */
+
+  const F = function () { this.x = 'X' }
+  F.prototype.y = 'Y'
+
+  const obj = { x: 1, y: 2 }
+
+  it('simple', () => {
+    expect(R.keys(obj)).eql([ 'x', 'y' ])
+    expect(R.keys(new F())).eql([ 'x' ])
+  })
+})
+
+describe('keysIn', () => {
+  /**
+   * keys only return properties, with prototype
+   */
+
+  const F = function () { this.x = 'X' }
+  F.prototype.y = 'Y'
+
+  const obj = { x: 1, y: 2 }
+
+  it('simple', () => {
+    expect(R.keysIn(obj)).eql([ 'x', 'y' ])
+    expect(R.keysIn(new F())).eql([ 'x', 'y' ])
+  })
+})
