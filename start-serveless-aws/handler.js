a301b6098526074f5bc745573636b237ae9c481e
David Jung <david.jung@peeper.co>
2018-08-23 11:16:43 +0900

    [serverless] GET "/" Endpoint 생성
    npx sls deploy

--- start-serveless-aws/handler.js
+++ start-serveless-aws/handler.js
@@ -1,16 +1,8 @@
-module.exports.run = (event, context, callback) => {
-  callback(null, "Hello World");
+module.exports.run = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Hello World!`
+    })
+  };
 };
-
-// module.exports.run = (event, context, callback) => {
-//   console.log("I'm a debug statement.")
-//   callback(null, "Hello World");
-// };
-
-// module.exports.run = (event) => {
-//   return Promise.resolve("Hello");
-// };
-
-// module.exports.run = async event => {
-//   return "Hello";
-// };
