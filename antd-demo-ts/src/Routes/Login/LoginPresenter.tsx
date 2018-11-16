23cde54a5ba9e4a909f8135ea0f849b4df9bfc9d
kanziw <kanziwoong@gmail.com>
2018-11-16 23:56:16 +0900

    [antd] Add Login Presenter

--- antd-demo-ts/src/Routes/Login/LoginPresenter.tsx
+++ antd-demo-ts/src/Routes/Login/LoginPresenter.tsx
@@ -1,5 +1,36 @@
-import React from 'react'
+import { Button, Form, Icon, Input } from 'antd'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
 
-const LoginPresenter: React.FunctionComponent = () => <h1>LOGIN REQUIRED!</h1>
+const StyleInputAlpha = { color: 'rgba(0,0,0,.25)' }
+
+const LoginPresenter: React.FunctionComponent = () => {
+  const [ token, setToken ] = useState('')
+  const onTokenInputChange = (e: ChangeEvent<HTMLInputElement>) => setToken(e.target.value)
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+  }
+
+  return (
+    <Form onSubmit={handleSubmit} className="login-form">
+      <Form.Item> Log in required! </Form.Item>
+      <Form.Item>
+        <Input
+          prefix={<Icon type="lock" style={StyleInputAlpha} />}
+          type="password"
+          placeholder="Github Token"
+          value={token}
+          onChange={onTokenInputChange}
+          onPressEnter={handleSubmit}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+}
 
 export default LoginPresenter
