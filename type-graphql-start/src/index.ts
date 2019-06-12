8938424ef24bd010ea69fbd595156c24c62c7f6c
kanziw <kanziwoong@gmail.com>
2019-06-12 22:37:11 +0900

    [type-graphql] Add forgot and change password

--- type-graphql-start/src/index.ts
+++ type-graphql-start/src/index.ts
@@ -7,17 +7,13 @@ import 'reflect-metadata'
 import { buildSchema, formatArgumentValidationError } from 'type-graphql'
 import { createConnection } from 'typeorm'
 
-import { ConfirmUserResolver } from './modules/user/ConfirmUser'
-import { LoginResolver } from './modules/user/Login'
-import { MeResolver } from './modules/user/Me'
-import { RegisterResolver } from './modules/user/Register'
 import { redis } from './redis'
 
 const main = async () => {
   await createConnection()
 
   const schema = await buildSchema({
-    resolvers: [ MeResolver, RegisterResolver, LoginResolver, ConfirmUserResolver ],
+    resolvers: [ __dirname + '/modules/**/*.ts' ],
   })
 
   const apolloServer = new ApolloServer({
--- type-graphql-start/src/modules/user/ConfirmUser.ts
+++ type-graphql-start/src/modules/user/ConfirmUser.ts
@@ -1,13 +1,14 @@
 import { Arg, Mutation, Resolver } from 'type-graphql'
-import { User } from '../../entity/User'
 
+import { User } from '../../entity/User'
 import { redis } from '../../redis'
+import { confirmUserPrefix } from '../constants/redisPrefixes'
 
 @Resolver()
 export class ConfirmUserResolver {
   @Mutation(() => Boolean)
   async confirmUser(@Arg('token') token: string): Promise<boolean> {
-    const userId = await redis.get(token)
+    const userId = await redis.get(confirmUserPrefix + token)
 
     if (!userId) {
       return false
